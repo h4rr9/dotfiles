4092c3f7a4cc1c6d188c45e96746ab65b6ec9011
Hariganesh Srinivasan <hari.ganesh.friendship@gmail.com>
2020-06-09 12:56:50 +0530

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,41 +1,40 @@
 -- IMPORTS
 
-import XMonad
 import Data.Monoid
 import System.Exit
-import XMonad.Util.SpawnOnce
-import XMonad.Util.Run
-import XMonad.Hooks.ManageDocks
+import XMonad
 import XMonad.Actions.CopyWindow (kill1, killAllOtherCopies)
-import XMonad.Actions.WithAll (sinkAll, killAll)
-import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
-import XMonad.Layout.WindowArranger
-import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
-import XMonad.Layout.Reflect (REFLECTX(..), REFLECTY(..))
-import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
-import XMonad.Layout.Spacing
-import XMonad.Layout.LayoutModifier
-import XMonad.Layout.Tabbed
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
-import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
-import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.GridVariants (Grid(Grid))
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.Spiral
-import XMonad.Layout.ThreeColumns
 import XMonad.Actions.MouseResize
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ServerMode
+import XMonad.Layout.Decoration
+import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
 import XMonad.Layout.NoBorders
 import XMonad.Layout.NoFrillsDecoration
-import XMonad.Layout.Decoration
+import XMonad.Layout.Reflect (REFLECTX(..), REFLECTY(..))
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
+import XMonad.Layout.ResizableTile
 import XMonad.Layout.Simplest
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ServerMode
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageHelpers
-
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spacing
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.WindowArranger
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
+import qualified Data.Map as M
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import qualified XMonad.StackSet as W
 
 ---------------------------------
 --VARIABLES
@@ -58,7 +57,7 @@ myClickJustFocuses :: Bool
 myClickJustFocuses = False                                  -- Uses first click on window to select window
 
 myBorderWidth :: Dimension
-myBorderWidth = 3                                           -- Sets border width for windows
+myBorderWidth = 4                                           -- Sets border width for windows
 
 myModMask :: KeyMask
 myModMask = mod4Mask
@@ -67,7 +66,7 @@ myNormalBorderColor :: [Char]
 myNormalBorderColor  = "#000000"                            -- Border colour of normal windows
 
 myFocusedBorderColor :: [Char]
-myFocusedBorderColor = "#000000"
+myFocusedBorderColor = "#268bd2"
 
 ------------------------------------------------------------------------
 --WORKSPACES
@@ -81,10 +80,10 @@ xmobarEscape = concatMap doubleLts
 
 myWorkspaces :: [String]
 myWorkspaces    = clickable . (map xmobarEscape)
-                  $ ["dev","www","sys","doc","vbox","chat","mus","vid","gfx"]
+                  $ ["dev","www","sys","doc","chat","mus","vid"]
     where
         clickable l = [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
-                      (i,ws) <- zip [1..9] l,                                        
+                      (i,ws) <- zip [1..7] l,                                        
                       let n = i ] 
 
 windowCount :: X (Maybe String)
@@ -134,17 +133,12 @@ myKeys =
     , ("M-<KP_Divide>", sendMessage (IncMasterN (-1)))  -- Decrease number of clients in master pane
 
 
-
-
-
-
     , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute")
     , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+ unmute")
     , ("<XF86AudioMute>", spawn "amixer sset Master toggle")
     , ("<XF86AudioPlay>", spawn "/usr/bin/python3 -m spotifycli --playpause") 
     , ("<XF86AudioPrev>", spawn "/usr/bin/python3 -m spotifycli --prev")
     , ("<XF86AudioNext>", spawn "/usr/bin/python3 -m spotifycli --next")
-
 
 
     ] 
@@ -183,18 +177,18 @@ mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 mySpacing' :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
-topBarTheme = def
-    { fontName              = myFont
-    , inactiveBorderColor   = "#002b36"
-    , inactiveColor         = "#002b36"
-    , inactiveTextColor     = "#002b36"
-    , activeBorderColor     = "#268bd2"
-    , activeColor           = "#268bd2"
-    , activeTextColor       = "#268bd2"
-    , urgentBorderColor     = "#dc322f"
-    , urgentTextColor       = "#c58900"
-    , decoHeight            = 10
-    }
+--topBarTheme = def
+    --{ fontName              = myFont
+    --, inactiveBorderColor   = "#002b36"
+    --, inactiveColor         = "#002b36"
+    --, inactiveTextColor     = "#002b36"
+    --, activeBorderColor     = "#268bd2"
+    --, activeColor           = "#268bd2"
+    --, activeTextColor       = "#268bd2"
+    --, urgentBorderColor     = "#dc322f"
+    --, urgentTextColor       = "#c58900"
+    --, decoHeight            = 10
+    --}
 
 
 myTabConfig = def { fontName            = myFont
@@ -206,19 +200,20 @@ myTabConfig = def { fontName            = myFont
                     , inactiveTextColor   = "#657b83"
                     }
 
+
 tall     = renamed [Replace "tall"]
-           $ noFrillsDeco shrinkText topBarTheme
+           -- $ noFrillsDeco shrinkText topBarTheme
            $ limitWindows 12
            $ mySpacing 8
            $ ResizableTall 1 (3/100) (1/2) []
 grid     = renamed [Replace "grid"]
-           $ noFrillsDeco shrinkText topBarTheme
+           -- $ noFrillsDeco shrinkText topBarTheme
            $ limitWindows 12
            $ mySpacing 8
            $ mkToggle (single MIRROR)
            $ Grid (16/10)
 threeCol = renamed [Replace "threeCol"]
-           $ noFrillsDeco shrinkText topBarTheme
+           -- $ noFrillsDeco shrinkText topBarTheme
            $ limitWindows 7
            $ mySpacing' 4
            $ ThreeCol 1 (3/100) (1/2)
@@ -226,7 +221,8 @@ monocle  = renamed [Replace "monocle"]
            $ limitWindows 20
            $ Full
 floats   = renamed [Replace "floats"]
-           $ noFrillsDeco shrinkText topBarTheme
+           -- $ noFrillsDeco shrinkText topBarTheme
+           -- $ simpleDeco shrinkText myfloatConfig
            $ limitWindows 20
            $ simplestFloat
 
